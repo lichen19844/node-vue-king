8f8040e105d37273dfd4530242f8cec7f3be993a
lichen <119449234@qq.com>
2020-01-23 23:29:42 +0800

    eslint console.log

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,9 +6,9 @@ module.exports = app => {
   const Category = require('../../models/Category')
   // 加一个post方法，接口地址是/categories，async函数是写一些执行东西
   router.post('/categories', async (req, res) => {
-    // 使用数据库及其模型，使用Category.create方法创建数据，数据来源是客户端提交过来的数据存进去
+    // 使用数据库及其模型，使用Category.create方法创建数据，数据来源是客户端提交过来的数据,存进Mongodb
     const model = await Category.create(req.body)
-    // 发回给客户端，让客户端知道数据库模型Category创建完成和创建的数据
+    // 发回给客户端（但是客户端在哪里接收它呢？），让客户端知道数据库模型Category创建完成和创建的数据
     res.send(model)
   })
   app.use('/admin/api', router)
