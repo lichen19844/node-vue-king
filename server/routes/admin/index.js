83d2e772699c0137127db32571939b503582013b
lichen <119449234@qq.com>
2020-02-03 00:33:43 +0800

    common crud

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -2,57 +2,77 @@
 module.exports = app => {
   const express = require('express')
   // express的子路由，用于增删改查
-  const router = express.Router()
+  const router = express.Router({
+    mergeParams: true
+  })
   /*
-  Category 就是 mongoose.model('Category', schema)  即
+  req.Model 就是 mongoose.model('req.Model', schema)  即
   
-  mongoose.model('Category', new mongoose.Schema({
+  mongoose.model('req.Model', new mongoose.Schema({
     name: { type: String },
-    parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }
+    parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'req.Model' }
   })).create({ parent: xxxxid, name: xxxname })
 
 	最后返回的数据结构如下：
   { _id: xxxnewid, name: xxxname, parent: xxxxid }
   */
-  const Category = require('../../models/Category')
 
-  // 加一个post方法，接口地址是/categories，async函数是写一些执行东西，比如把数据存进数据库
-  router.post('/categories', async (req, res) => {
-    // 使用数据库及其模型，使用Category.create方法创建数据，数据来源是客户端submit提交过来的数据,存进Mongodb
-    const model = await Category.create(req.body)
+  // const req.Model = require('../../models/req.Model')
+
+  // 加一个post方法，接口地址是，async函数是写一些执行东西，比如把数据存进数据库
+  router.post('/', async (req, res) => {
+    // const Model = require(`../../models/${req.params.resource}`)
+    // 使用数据库及其模型，使用req.Model.create方法创建数据，数据来源是客户端submit提交过来的数据,存进Mongodb
+    // const model = await req.Model.create(req.body)
+    const model = await req.Model.create(req.body)
     console.log(model)
-    // 发回给前端客户端（但是客户端在哪里接收它呢？可以在rest client插件里看到返回），让客户端知道数据库模型Category创建完成和创建的数据
+    // 发回给前端客户端（但是客户端在哪里接收它呢？可以在rest client插件里看到返回），让客户端知道数据库模型req.Model创建完成和创建的数据
     res.send(model)
   })
 
-  router.put('/categories/:id', async (req, res) => {
+  router.put('/:id', async (req, res) => {
     console.log('req.body is ', req.body)
     // req.params.id能拿到/categories/:id接到的id值
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     // res.send(model)
     model
     res.send('model')
   })
 
-  router.delete('/categories/:id', async (req, res) => {
-    await Category.findByIdAndDelete(req.params.id, req.body)
+  router.delete('/:id', async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true
     })
   })
 
-  router.get('/categories', async (req, res) => {
-    // const items = await Category.find().limit(10)
-    const items = await Category.find().populate('parent').limit(10)
-    // 发送给前端客户端
+  router.get('/', async (req, res) => {
+    let queryOptions = {}
+    if (req.Model.modelName === 'Category') {
+      queryOptions.populate = 'parent'
+      // queryOptions.populate = {path: 'parent'}
+    }
+    // const items = await req.Model.find().populate('parent').limit(10)
+    // const items = await req.Model.find().populate({path: 'parent'}).limit(10)
+    const items = await req.Model.find().setOptions(queryOptions).limit(10)
+    console.log('queryOptions is ', queryOptions)
     res.send(items)
   })
 
-  router.get('/categories/:id', async (req, res) => {
-    const model = await Category.findById(req.params.id)
-    // 发送给前端客户端
+  router.get('/:id', async (req, res) => {
+    const model = await req.Model.findById(req.params.id)
     res.send(model)
   })  
 
-  app.use('/admin/api', router)
+  // app.use是这个文件中第一个执行的方法，先匹配路径，再执行后面的中间件，再执行next即router
+  app.use('/admin/api/rest/:resource', async (req, res, next) => {
+    // 将父级接收的:resource参数转成模型的名称(首字母大写单数)
+    const modelName = require('inflection').classify(req.params.resource)
+    // 挂载Model到请求对象req上成为一个属性，router需要用到它时可以从全局的req上取到。不能使用const Model，因为后面的router访问不到
+    req.Model = require(`../../models/${modelName}`)
+    console.log(req)
+    console.log('req is contain Model ', req.Model)
+    console.log('req is contain Model ', req.Model.modelName)
+    next()
+  } ,router)
 }
