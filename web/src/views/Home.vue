4ba321213cddc538c0c57942b0c8b4da8c82a34d
lichen <119449234@qq.com>
2020-02-21 16:36:16 +0800

    sprite

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -13,7 +13,20 @@
       <div class="swiper-pagination pagination-home text-right px-3 pb-2"  slot="pagination"></div>
     </swiper>
     <!-- end of swiper -->
-    
+
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-gray-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3"
+        v-for="item in 10" :key="item">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm d-flex jc-center">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -50,4 +63,17 @@ export default {
       }
     }
   }
+
+  .nav-icons {
+    border-top: 1px solid $border-color;
+    border-bottom: 1px solid $border-color;
+    .nav-item {
+      width: 25%;
+      border-right: 1px solid $border-color;
+      & :nth-child(4n) {
+        border-right: none;
+      }
+    }
+  }
+  
 </style>
