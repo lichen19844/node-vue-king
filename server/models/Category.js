f543c5d7b36f6893d58ee8bab7c9637707251c58
lichen <119449234@qq.com>
2020-02-29 00:32:43 +0800

    schema children populate

--- server/models/Category.js
+++ server/models/Category.js
@@ -5,5 +5,26 @@ const schema = new mongoose.Schema({
   parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }
 })
 
+schema.virtual('children', {
+  localField: '_id',
+  foreignField: 'parent',
+  justOne: false,
+  ref: 'Category'
+})
+
+schema.virtual('newsList', {
+  localField: '_id',
+  foreignField: 'categories',
+  justOne: false,
+  ref: 'Article'
+})
+
+// schema.virtual('grandChildren', [{
+//   localField: '_id',
+//   foreignField: 'parent',
+//   justOne: false,
+//   ref: 'Category'
+// }])
+
 // 哪里需要用到这个模型，就在哪里引用
 module.exports = mongoose.model('Category', schema)
